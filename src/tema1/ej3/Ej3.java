8c535fddc69667363bf0e673a4bb021c1f9aff8c
mariosinnadamas <mariosaeztellez@gmail.com>
2025-09-29 10:12:21 +0200

    Ejercicio 1.3 hecho

---
+++ src/tema1/ej3/Ej3.java
@@ -0,0 +1,39 @@
+package tema1.ej3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ej3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1;
+        int n2;
+        int resultado;
+        String in;
+
+        while (true){
+            System.out.println("Introduce el primer numero: ");
+            try{
+                in = sc.nextLine();
+                n1 = Integer.parseInt(in);
+                while(true){
+                    System.out.println("Introduce el segundo numero: ");
+                    in = sc.nextLine();
+                    try{
+                        n2 = Integer.parseInt(in);
+                        break;
+
+                    } catch (NumberFormatException e){
+                        System.err.println("ERROR: Introduce un numero entero" + e.getMessage());
+                    }
+                }
+                break;
+            } catch (NumberFormatException e){
+                System.err.println("ERROR: Introduce un numero entero" + e.getMessage());
+            }
+        }
+        resultado = n1 + n2;
+        System.out.println("Resultado: " + resultado);
+    }
+}
---
+++ src/tema1/ej3/Ej3B.java
@@ -0,0 +1,48 @@
+package tema1.ej3;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Ej3B {
+    public static void main(String[] args) throws IOException {
+        File f = new File("src");
+        ProcessBuilder pb = new ProcessBuilder("java", "tema1.ej3.Ej3");
+        pb.directory(f);
+        Process p = pb.start();
+        //En este output sacas la información de este archivo (Por eso es out y no in), en este caso estamos sacando el 2 y 3
+        OutputStream os = p.getOutputStream();
+        os.write("2\n".getBytes()); //El \n nos simula haber dado al enter
+        os.write("3\n".getBytes());
+        os.flush();
+        os.close(); //Importante cerrar siempre
+
+        //Lectura
+        try {
+            //Aqui es input porque recibe los datos de esta clase, es decir, los introduce
+            InputStream is = p.getInputStream();
+            InputStream err = p.getErrorStream(); //Importante para poder leer el fallo del proceso hijo
+            int c;
+            while ((c = is.read())!=-1){
+                System.out.print((char) c);
+            }
+            //Esto te permite leer el error del proceso hijo
+            while ((c = err.read())!=-1){
+                System.err.print((char) c);
+            }
+            is.close();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        //comprobacion de error: 0 bien | 1 mal
+        int exitVal;
+        try {
+            exitVal = p.waitFor();
+            System.out.println("Valor de salida: " + exitVal);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
